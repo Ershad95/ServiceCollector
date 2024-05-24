f3c8ad315c69d76f3cf02df023e1ca00ced819ff
Ershad <ershadRaoufi@gmail.com>
2024-05-24 04:48:45 -0700

    add mock service contract to abstractions.

---
+++ ServiceCollector.Abstractions/IMockService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceCollector.Abstractions;
+
+public interface IMockService
+{
+    public IServiceCollection MockedServiceCollection { get; }
+}
