f035b4f7d276c3958e4556ea929105463af168d8
Ershad <ershadRaoufi@gmail.com>
2024-05-24 04:49:27 -0700

    add some extensions for IMockService.

---
+++ ServiceCollector.Mock/IMockServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using ServiceCollector.Abstractions;
+
+namespace ServiceCollector.Mock;
+
+public static class MockServiceExtensions
+{
+    public static TServiceType? GetService<TServiceType>(this IMockService mockService) where TServiceType : class
+    {
+        var serviceDescriptor =
+            mockService.MockedServiceCollection.FirstOrDefault(x => x.ServiceType == typeof(TServiceType));
+        return serviceDescriptor?.ImplementationInstance as TServiceType;
+    }
+
+    public static IList<TServiceType?> GetServices<TServiceType>(this IMockService mockService)
+        where TServiceType : class
+    {
+        var serviceDescriptor = mockService.MockedServiceCollection.Where(x => x.ServiceType == typeof(TServiceType));
+        return serviceDescriptor.Select(x => x.ImplementationInstance as TServiceType).ToList();
+    }
+}
